03c2358c9f97d11b008f7fabd23efa2c589246c4
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2020-09-03 03:43:45 -0300

    fix: primeiras correcoes no estilo do login

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,38 +2,54 @@ import Head from 'next/head'
 
 export default function Login() {
   return (
-    <div className="bg-bgMain h-screen w-screen flex items-center justify-center">
+	<div 
+		className="bg-bgMain p-4 min-h-screen min-w-full flex flex-col items-center justify-center"
+	>
       <Head>
         <title>Login</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-			<header className="w-472 h-122">
-				<div className="mb-10 mt-10 text-4xl text-black">
-        	<span className="text-sm">11º FESTIVAL MAESTRO</span>
-          <h1 className="font-extrabold">FELINTO LÚCIO DANTAS</h1>
-      	</div>
+		  	<header className="mb-10 text-4xl text-black">
+				<span className="text-sm">11º FESTIVAL MAESTRO</span>
+				<h1 className="font-extrabold">FELINTO LÚCIO DANTAS</h1>
 			</header>
 			
-			<form className="w-359 h-359 bg-white rounded">
-				<div className="w-286 h-86">
-					<label className="mb-4"htmlFor="email">Email</label>
-					<div></div>
-					<input className="border-solid border-2 rounded w-full h-45"type="email" placeholder="Email"/>
+			<form className="bg-white w-screen sm:max-w-sm p-4 rounded">
+				<div className="flex flex-col">
+					<label className="mb-4" htmlFor="email">Email</label>
+					<input 
+						className="border-solid border-2 px-4 py-2 rounded"
+						type="email"
+						placeholder="Email"
+					  	required
+					/>
 				</div>
-				<div className="w-286 h-86">
+			  	
+				<div className="flex flex-col mt-4">
 					<label className="mb-4" htmlFor="senha">Senha</label>
-					<div></div>
-					<input className="border-solid border-2 rounded w-full h-45" type="password" placeholder="***********"/>
+					<input 
+					  	className="border-solid border-2 px-4 py-2 rounded"
+						type="password"
+						placeholder="***********"
+						required
+					/>
 				</div>
 
-				<button className="bg-strongOrange rounded">Entrar</button>
-				<a className="text-strongOrange"href="#">Esqueceu?</a>
-			</form>
+				<div className="mt-4 flex items-center justify-between">
+					<button className="bg-orange-500 px-4 py-2 rounded font-bold">
+						Entrar
+					</button>
+				  	<a className="text-orange-700" href="#">Esqueceu?</a>
+				</div>
 
-			<button className="bg-strongOrange rounded">Quero me inscrever</button>
+			</form>
+		
+			<button className="bg-orange-500 w-full sm:max-w-sm font-bold px-4 py-2 mt-10 rounded">Quero me inscrever</button>
 	
-			<footer className="text-strongOrange">© 2020 Associação Musical de Santa Cruz. Todos os direitos reservados.</footer>
+			<footer className="text-orange-700 text-center mt-8">
+				© 2020 Associação Musical de Santa Cruz. Todos os direitos reservados.
+			</footer>
     </div>
   )
 }
