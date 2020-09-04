d70c8ba2233f148893a6b73650e3ff97616a9acc
PauloVLB <vitorlimaborges@gmail.com>
2020-09-04 13:32:53 -0300

    feito handleInputChange

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,36 @@
 import Head from 'next/head'
+import { useState } from 'react'
+
+const ImagensInstrumento = () => (
+    <>
+        <img 
+            src="/assets/sax.png"
+            alt="Saxofone"
+            className="absolute top-0 left-0 w-64 sm:w-1/4" 
+        />
+        <img
+            src="/assets/trompete.png"
+            alt="Trompete"
+            className="absolute bottom-0 right-0 w-56 sm:w-1/4"
+        />
+    </>
+)
 
 export default function Login() {
+	const [formData, setFormData] = useState({
+		email: '',
+		senha: ''
+	})
+
+	function handleInputChange(e) {
+        const { name, value } = e.target
+        
+        setFormData({ ...formData, [name]: value })
+	}
+	
+	function handleSubmit(e) {
+		e.preventDefault()
+	}
   return (
 	<div 
 		className="bg-bgMain p-4 min-h-screen min-w-full flex flex-col items-center justify-center"
@@ -9,8 +39,8 @@ export default function Login() {
         <title>Login</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-		  	<header className="mb-10 text-4xl text-black">
+		<ImagensInstrumento/>
+			<header className="mb-10 text-4xl text-black">
 				<span className="text-sm">11º FESTIVAL MAESTRO</span>
 				<h1 className="font-extrabold">FELINTO LÚCIO DANTAS</h1>
 			</header>
@@ -22,6 +52,7 @@ export default function Login() {
 						className="border-solid border-2 px-4 py-2 rounded"
 						type="email"
 						placeholder="Email"
+						onChange={handleInputChange}
 					  	required
 					/>
 				</div>
@@ -37,9 +68,9 @@ export default function Login() {
 				</div>
 
 				<div className="mt-4 flex items-center justify-between">
-					<button className="bg-orange-500 px-4 py-2 rounded font-bold">
-						Entrar
-					</button>
+					<input className="bg-orange-500 px-4 py-2 rounded font-bold" value="Entrar" type="submit">
+						
+					</input>
 				  	<a className="text-orange-700" href="#">Esqueceu?</a>
 				</div>
 
