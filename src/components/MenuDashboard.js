a69629c256057d29c0e6d3032467c195b358011e
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2020-09-07 18:23:17 -0300

    fix: --module ++links centralizados

--- src/components/MenuDashboard.js
+++ src/components/MenuDashboard.js
@@ -1,33 +1,32 @@
 import Link from 'next/link'
-import styles from '../../styles/MenuDashboard.module.css'
 
 const MenuDashboard = () => (
     <>
-        <header className="bg-redHeader h-screen w-56 flex flex-col">
-            <div className="bg-redHeaderStrong items-center">
-                <Link href="/dashboard">
-                   <a className="flex flex-row justify-between mt-3 mb-3">
-                       <img src="assets/grid.svg" className="inline ml-8"/>
-                       <h2 className={styles.dashboard_h2}>Dashboard</h2>
-                   </a>
-                </Link>
-            </div>
-            <div className="bg-redHeader items-center">
-                <Link href="/dashboard">
-                   <a className="flex flex-row justify-between mt-3 mb-3">
-                       <img src="assets/user.svg" className="inline ml-8"/>
-                       <h2 className={styles.perfil_h2}>Perfil</h2>
-                   </a>
-                </Link>
-            </div>
-            <div className="bg-redHeaderStrong items-center">
-                <Link href="/dashboard">
-                   <a className="flex flex-row justify-between mt-3 mb-3">
-                       <img src="assets/trello.svg" className="inline ml-8"/>
-                       <h2 className={styles.oficinas_h2}>Minhas Oficinas</h2>
-                   </a>
-                </Link>
-            </div>
+        <header className="bg-redHeader h-screen w-64 flex flex-col">
+            <Link href="/dashboard">
+                <a className="bg-redHeaderStrong flex items-center justify-center py-4 px-6">
+                    <div className="flex flex-row items-center w-40">
+                        <img src="assets/grid.svg" className="" />
+                        <h2 className="ml-4 text-white font-bold">Dashboard</h2>
+                    </div>
+                </a>
+            </Link>
+            <Link href="/dashboard">
+                <a className="bg-redHeader flex items-center justify-center py-4 px-6">
+                    <div className="flex flex-row items-center w-40">
+                        <img src="assets/user.svg" className="" />
+                        <h2 className="ml-4 text-white font-bold">Meu perfil</h2>
+                    </div>
+                </a>
+            </Link>
+            <Link href="/dashboard">
+                <a className="bg-redHeaderStrong flex items-center justify-center py-4 px-6">
+                    <div className="flex flex-row items-center w-40">
+                        <img src="assets/trello.svg" className="" />
+                        <h2 className="ml-4 text-white font-bold">Minhas oficinas</h2>
+                    </div>
+                </a>
+            </Link>
             
         </header>
     </>
