5ce4a4cf25d4eca4d7b94b617e1f24a4388681c0
PauloVLB <vitorlimaborges@gmail.com>
2020-09-08 05:31:40 -0300

    Feito teste inicial do endpoint de chamadas

--- src/pages/api/chamadas.js
+++ src/pages/api/chamadas.js
@@ -10,13 +10,34 @@ const COLLECTION_NAME = 'participantes'
 handler.use(dbMiddleware)
 
 handler.put(async (req, res) => {
-    const { oficina } = req.body
-    
-    const participantes = await req.db.collection(COLLECTION_NAME)
-        .find({ 
-            "oficinas": { $all: [oficina] } 
-        }).toArray()
+    const { oficina, idParticipantes } = req.body
+ 
+    idParticipantes.forEach(async idParticipante => { 
+        let participante = await req.db.collection(COLLECTION_NAME).findOne({ "_id": ObjectID(idParticipante) })
+        let newPresencaOficinas
+        if(!participante.hasOwnProperty('presencaOficinas')) {
+            newPresencaOficinas = new Map();
+            newPresencaOficinas.set(oficina, 1)
+            console.log("não tinha o campo")
+        } else {
+            newPresencaOficinas = participante.presencaOficinas
+            if(newPresencaOficinas[oficina]) {
+                newPresencaOficinas[oficina]++
+            } else {
+                newPresencaOficinas[oficina] = 1
+            }
+            console.log("tinha campo")
+        }
 
+        await req.db.collection(COLLECTION_NAME).findOneAndUpdate(
+            { "_id" : ObjectID(idParticipante) },
+            { $set: { presencaOficinas: newPresencaOficinas } }
+        )
+        
+        console.log("cabo")
+
+        return
+    })
     
     return
 })
