d5330dab49c75e6cef21b9738b630674463698c9
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2020-09-12 23:48:26 -0300

    fix(api): quantidade-participantes db import

--- src/pages/api/oficinas/quantidade-participantes.js
+++ src/pages/api/oficinas/quantidade-participantes.js
@@ -1,5 +1,5 @@
 import nextConnect from 'next-connect'
-import dbMiddleware from '../../middlewares/database'
+import dbMiddleware from '../../../middlewares/database'
 
 const handler = nextConnect()
 
