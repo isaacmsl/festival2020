85cf7ceea1edf1a51b90893a54541f1926a540c9
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2020-09-15 20:01:26 -0300

    feat(front): oficinas card

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -75,7 +75,7 @@ export default function Dashboard({participante, professores}) {
     }
 
     return (
-        <div className="flex flex-col sm:flex-row">
+        <div className="flex flex-col sm:flex-row overflow-auto">
             <Head>
                 <title>Festival 2020</title>
                 <link rel="icon" href="/favicon.ico" />
@@ -92,31 +92,32 @@ export default function Dashboard({participante, professores}) {
                 </header>
 
                 <div className="font-normal bg-orange-200 p-4 border border-solid mb-8 max-w-2xl border-gray-200 rounded">
-                    <p>
-                        <b>CERTIFICADOS:</b> os participantes deverão possuir 100% de presença nas oficinas para poderem ganhar os certificados. A previsão de disponibilidade é até o dia de domingo (20/09).
+                    <p className="text-justify">
+                        <b>Certificados:</b> os participantes deverão possuir 100% de presença nas oficinas para poderem ganhar os certificados. A previsão de disponibilidade é até o dia de domingo (20/09).
                         </p>
                 </div>
 
-                <div className="font-normal bg-orange-200 p-4 border border-solid mb-8 max-w-2xl border-gray-200 rounded">
-                    <p>
-                        <b>Por favor, nos ajude a melhorar o nosso festival enviando seu feedback</b>
-                        <ul>
-                            <li>Alguma ideia para melhorarmos as aulas, plataforma ou o festival em sí?</li>
-                            <li>Estás passando por algum problema?</li>
-                            <li>Alguma dúvida sobre como usar a plataforma?</li>
-                            <li>Alguma informação parece incorreta?</li>
-                        </ul>
-                    </p>
-                    <form className="bg-white w-screen sm:max-w-sm p-4 rounded" onSubmit={handleFeedback}>
+                <div className="font-normal bg-orange-200 p-4 border border-solid mb-8 max-w-2xl border-gray-200 rounded flex flex-col gap-4">
+                    <h2 className="font-bold">
+                        Por favor, nos ajude a melhorar o festival enviando seu feedback
+                    </h2>
+                    <ul className="flex flex-col gap-2">
+                        <li>Alguma ideia para melhorarmos as aulas, plataforma ou o festival em geral?</li>
+                        <li>Estás passando por algum problema?</li>
+                        <li>Alguma dúvida sobre como usar a plataforma?</li>
+                        <li>Alguma informação parece incorreta?</li>
+                    </ul>
+                    <form className="w-full rounded" onSubmit={handleFeedback}>
                         <div className="flex flex-col mt-4">
-                            <label className="mb-4" htmlFor="feedbackInput">Feedback</label>
+                            <label className="mb-4 font-bold" htmlFor="feedbackInput">Feedback</label>
                             <textarea
                                 id="feedbackInput"
-                                className="border-solid border-2 px-4 py-2 rounded"
+                                className="w-full border-solid border-2 px-4 py-2 rounded"
                                 name="feedback"
                                 placeholder="Seu feedback é muito importante..."
                                 rows="4"
                                 cols="50"
+                                maxLength={300}
                                 onChange={e => setFeedback(e.target.value)}
                                 required
                             />
@@ -124,7 +125,7 @@ export default function Dashboard({participante, professores}) {
 
                         <div className="mt-4 flex items-center justify-between">
                             <input
-                                className="bg-orange-500 px-6 py-4 rounded font-bold"
+                                className="px-6 py-4 bg-blue-600 text-white font-bold rounded text-center w-full"
                                 value="Enviar feedback"
                                 type="submit"
                             />
@@ -132,11 +133,11 @@ export default function Dashboard({participante, professores}) {
                     </form>
                 </div>
 
-                <main className="flex flex-wrap gap-4">
-
+                <main className="w-full max-w-2xl grid grid-cols-1 md:grid-cols-2 gap-4">
                     {oficinas.map((oficina, index) => (
                         <Link as={`/dashboard/oficinas/${oficina.nome}`} href="/dashboard/oficinas/[oficina]" key={index}>
-                            <a className="w-full">
+                            {/* <a className="w-full sm:max-w-xs"> */}
+                            <a className="cursor-pointer">
                                 <OficinaCard autorizacao={participante.autorizacao} oficina={oficina}  />
                             </a>
                         </Link>
