ffcfd72bdb6cf668362abeab9b8b307dbaf4cf71
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2021-09-07 21:18:28 -0300

    fix: remove hotjar heatmap script

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,17 +18,6 @@ export default function Home({ diasRestantes }) {
       <Head>
         <title>Festival - 2020</title>
         <link rel="icon" href="/favicon.ico" />
-        <!-- Hotjar Tracking Code for https://festivalmfld2020.vercel.app/ -->
-        <script>
-            (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:2052202,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        </script>
       </Head>
 
       <HeaderDivulgacao />
